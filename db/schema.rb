7489577572c8534553d4b047d0d1f4627722f564
Losangelo Soares de Sousa Pacífico <losangelo@gmail.com>
2012-02-15 11:20:41 -0300

    alteracoes em rodape, add equipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,44 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120124193829) do
+ActiveRecord::Schema.define(:version => 20120214141305) do
+
+  create_table "equipes", :force => true do |t|
+    t.string   "setor"
+    t.string   "descricao"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "institucionals", :force => true do |t|
     t.string   "servico"
     t.string   "descricao"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orcamentos", :force => true do |t|
+    t.string   "razao"
+    t.string   "cpf"
+    t.string   "insc_social"
+    t.string   "telefone"
+    t.string   "email"
+    t.string   "endereco"
+    t.string   "bairro"
+    t.string   "cidade"
+    t.string   "uf"
+    t.string   "website"
+    t.integer  "quatidade"
+    t.string   "nome_produto"
+    t.integer  "numero_de_paginas_ou_vias"
+    t.decimal  "tamanho_em_centrimetros"
+    t.string   "cromia_ou_cores"
+    t.string   "papel_ou_substrato"
+    t.string   "gramatura"
+    t.string   "acabamento"
+    t.string   "outros_acabamentos"
+    t.string   "prazo_necessario"
+    t.text     "obs"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
